7f7061e6426f9ffa89b7690b33e7d2a0bbb03ec1
GeunoYu37 <137762389+carrot0109@users.noreply.github.com>
2024-01-10 14:20:20 +0900

    Update 240110_class_2.py

--- 240110_class_2.py
+++ 240110_class_2.py
@@ -12,7 +12,7 @@ import urllib.request
 url = "http://goo.gl/U7mSQl"    # 접속할 웹 페이지
 html = urllib.request.urlopen(url)  # 웹 페이지 열기
 html_contents = str(html.read())    # 웹 페이지의 내용을 문자열로
-id_results = re.findall("([A-Za-z0-9]+\*\*\*)",html_contents)
-# findall 저네 찾기, 정규 표현식 패턴대로 데이터 찾기
+id_results = re.findall(r"([A-Za-z0-9]+\*\*\*)",html_contents)
+# findall 전체 찾기, 정규 표현식 패턴대로 데이터 찾기
 for i in id_results:    
     print(i)
